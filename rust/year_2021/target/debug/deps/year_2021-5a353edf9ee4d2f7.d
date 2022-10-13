24b26ad627a9088beacc4ed2fb9d73f6bf42e067
Manny Sran <manny@modelliving.com>
2022-10-13 01:49:08 -0400

    Removing target folder

--- rust/year_2021/target/debug/deps/year_2021-5a353edf9ee4d2f7.d
+++
@@ -1,6 +0,0 @@
-/home/manny/advent-of-code/rust/year_2021/target/debug/deps/year_2021-5a353edf9ee4d2f7: src/main.rs src/problem_01.rs
-
-/home/manny/advent-of-code/rust/year_2021/target/debug/deps/year_2021-5a353edf9ee4d2f7.d: src/main.rs src/problem_01.rs
-
-src/main.rs:
-src/problem_01.rs:
--- rust/year_2021/target/debug/year_2021.d
+++
@@ -1,1 +0,0 @@
-/home/manny/advent-of-code/rust/year_2021/target/debug/year_2021: /home/manny/advent-of-code/rust/year_2021/src/main.rs /home/manny/advent-of-code/rust/year_2021/src/problem_01.rs
